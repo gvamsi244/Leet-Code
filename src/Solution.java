165beb122ca28fafd16cac5ae4c9d8bb2954b94b
gvams <gvamsi244@gmail.com>
2020-03-31 17:48:19 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,16 @@
+import java.lang.Math;
+public class Solution {
+    public static int hammingDistance(int x, int y) {
+        int z=x^y,count=0;
+        String str=Integer.toBinaryString(z);
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)=='1')
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1,4));
+    }
+}
