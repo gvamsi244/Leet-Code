5e8056f1f3b8b9d40804e4369e0a885d3245f56f
gvams <gvamsi244@gmail.com>
2020-05-17 19:40:24 -0400

    Problem: 26 Solved!

--- src/Solution26.java
+++ src/Solution26.java
@@ -1,11 +1,12 @@
 import java.util.*;
 
 public class Solution26 {
-    public static int removeDuplicates(int[] nums) {
+    public int removeDuplicates(int[] nums) {
         if((nums.length==0) || (nums.length==1))
             return nums.length;
 
         int pointer_1=0, new_number=nums[0];
+
         for(int pointer_2=1;pointer_2<nums.length;pointer_2++){
             if(nums[pointer_2]!=new_number)
             {
@@ -15,13 +16,5 @@ public class Solution26 {
             }
         }
         return ++pointer_1;
-    }
-
-    public static void main(String[] args) {
-        int [] arr = new int[]{ 0,0,1,1,1,2,2,3,3,4};
-        System.out.println(removeDuplicates(arr));
-        System.out.println("FInal Arrays :"+Arrays.toString(arr));
-
-
     }
 }
