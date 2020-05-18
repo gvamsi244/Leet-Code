484aad1779401d4e3e04d91d5321aa5291e22efe
gvams <gvamsi244@gmail.com>
2020-05-18 17:45:57 -0400

    Added Solution: 217

---
+++ src/Solution217.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/**
+ * @author Vamsi Krishna Govada on 5/18/2020.
+ * @project LeetCode
+ */
+
+//Contains Duplicate
+
+public class Solution217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++)
+            if (nums[i] == nums[i + 1])
+                return true;
+        return false;
+    }
+}
