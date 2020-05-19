376e7a658d1cd5259035f9cd70ad5acea690c6ff
gvams <gvamsi244@gmail.com>
2020-05-19 13:10:58 -0400

    Added Solution: 136

---
+++ src/Solution136.java
@@ -0,0 +1,27 @@
+/**
+ * @author Vamsi Krishna Govada on 5/18/2020.
+ * @project LeetCode
+ */
+
+// Single Number
+
+public class Solution136 {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if((nums.length==1))
+            return nums[0];
+        for(int i=1;i<nums.length;i=i+2){
+            if(nums[i-1]==nums[i]){
+                if((i+2)<nums.length)
+                    continue;
+                else
+                    return nums[nums.length-1];
+
+            }
+            else
+                return nums[i-1];
+        }
+
+        return 0;
+    }
+}
