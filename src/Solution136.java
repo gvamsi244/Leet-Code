ca1a9353d9012447af3fd39dd394a4541bf709be
gvams <gvamsi244@gmail.com>
2020-05-20 04:03:57 -0400

    Updated Solution: 136

--- src/Solution136.java
+++ src/Solution136.java
@@ -9,21 +9,11 @@ import java.util.Arrays;
 
 public class Solution136 {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        if((nums.length==1))
-            return nums[0];
-        for(int i=1;i<nums.length;i=i+2){
-            if(nums[i-1]==nums[i]){
-                if((i+2)<nums.length)
-                    continue;
-                else
-                    return nums[nums.length-1];
-
-            }
-            else
-                return nums[i-1];
+        int output=0;
+        int len = nums.length;
+        for(int i=0;i<len;i++){
+            output=output^nums[i];
         }
-
-        return 0;
+        return output;
     }
 }
