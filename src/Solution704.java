4d74abdf8f9e73be8815f8ef0a8fcfefe44d7e73
gvams <gvamsi244@gmail.com>
2020-06-02 15:16:43 -0400

    Added Solution: 704

---
+++ src/Solution704.java
@@ -0,0 +1,28 @@
+/**
+ * @author Vamsi Krishna Govada on 6/2/2020.
+ * @project LeetCode
+ */
+
+// Binary Search
+
+
+public class Solution704 {
+    public int search(int[] nums, int target) {
+        int len = nums.length;
+        int output = -1;
+        int start = 0, last = len - 1;
+        int mid = 0;
+        while (start <= last) {
+            mid = (start + last) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                last = mid - 1;
+            }
+        }
+        return output;
+    }
+}
