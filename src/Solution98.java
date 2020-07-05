826bccc5c4ef6ff5101aca01d10616c7ce0f21db
gvams <gvamsi244@gmail.com>
2020-07-05 16:17:21 -0400

    Added Solution: 98

---
+++ src/Solution98.java
@@ -0,0 +1,47 @@
+/**
+ * @author Vamsi Krishna Govada on 7/5/2020.
+ * @project LeetCode
+ */
+
+
+// Validate Binary Search Tree
+
+
+public class Solution98 {
+
+    // Definition for a binary tree node
+    // Comment this section in case if you need to submit as Solution in Leetcode
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, null, null);
+    }
+
+    public boolean helper(TreeNode root, Integer min, Integer max) {
+
+        if (root == null)
+            return true;
+        if ((min != null) && (root.val <= min))
+            return false;
+        if ((max != null) && (root.val >= max))
+            return false;
+        return ((helper(root.left, min, root.val) && helper(root.right, root.val, max)));
+    }
+}
