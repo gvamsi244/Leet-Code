1eb496bd64e389165469bc86311f48b0ac872cc6
gvams <gvamsi244@gmail.com>
2020-07-05 19:03:12 -0400

    Updated Indentation

--- src/Solution107.java
+++ src/Solution107.java
@@ -17,8 +17,14 @@ public class Solution107 {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
@@ -28,25 +34,25 @@ public class Solution107 {
 
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> output = new ArrayList<>();
-        if(root!=null)
-            helper(root,output);
+        if (root != null)
+            helper(root, output);
         return output;
     }
 
-    public void helper(TreeNode root, List<List<Integer>> output){
+    public void helper(TreeNode root, List<List<Integer>> output) {
 
         Queue<TreeNode> queue = new LinkedList();
         queue.add(root);
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             List<Integer> list = new ArrayList<>();
             int size = queue.size();
-            for(int i=0;i<size;i++){
+            for (int i = 0; i < size; i++) {
                 TreeNode temp = queue.peek();
                 queue.remove();
                 list.add(temp.val);
-                if(temp.left!=null) queue.add(temp.left);
-                if(temp.right!=null) queue.add(temp.right);
+                if (temp.left != null) queue.add(temp.left);
+                if (temp.right != null) queue.add(temp.right);
             }
 
             output.add(list);
