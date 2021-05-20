99f99f33abd725d89ebfd970099a741aa0245b70
Vamsi krishna <gvamsi244@gmail.com>
2021-05-19 21:46:55 -0500

    Added Solution:1025

---
+++ src/Solution1025.java
@@ -0,0 +1,15 @@
+/**
+ * @author Vamsi Krishna Govada on  5/18/2021.
+ * @project Leet-Code
+ */
+
+// Divisor Game
+
+public class Solution1025 {
+    public boolean divisorGame(int n) {
+        if(n%2==0)
+            return true;
+        else
+            return false;
+    }
+}
